d17e29f73b868c5398f86d32335c0b9581fb2070
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-11 15:16:57 +0000

    gave names to routes and started using them

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,16 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create" 
-  get "/movies/new" => "movies#new" 
+  post "/movies" => "movies#create", as: :movies # movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path
           
   # READ
   get "/movies" => "movies#index" 
-  get "/movies/:id" => "movies#show" 
+  get "/movies/:id" => "movies#show", as: :movie # movie_path() -> expects an argument to populate the :id segment
   
   # UPDATE
   patch "/movies/:id" => "movies#update" 
-  get "/movies/:id/edit" => "movies#edit" 
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie 
   
   # DELETE
   delete "/movies/:id" => "movies#destroy" 
